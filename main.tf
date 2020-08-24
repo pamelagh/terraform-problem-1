8c5ef960a46b730a475c59d05a903227e6f3b649
Pamela Garces <pamelag@codefabric.io>
2020-08-24 10:28:50 -0400

    update

--- main.tf
+++ main.tf
@@ -15,14 +15,14 @@ resource "aws_internet_gateway" "default" {
     vpc_id = aws_vpc.main.id
 }
 
-resource "aws_eip" "nat" {
-    vpc = true
-}
-
 resource "aws_subnet" "public" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "true"
+}
+
+resource "aws_eip" "nat" {
+    vpc = true
 }
 
 resource "aws_nat_gateway" "nat" {
@@ -45,21 +45,50 @@ resource "aws_subnet" "public-network-2" {
     map_public_ip_on_launch = "true"
 }
 
-resource "aws_route_table" "routes" {
+resource "aws_route_table" "public-routes" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.default.id
+    }
+}
+
+resource "aws_route_table_association" "public-1" {
+    subnet_id = aws_subnet.public-network-1.id
+    route_table_id = aws_route_table.public-routes.id
+}
+
+resource "aws_route_table_association" "public-2" {
+    subnet_id = aws_subnet.public-network-2.id
+    route_table_id = aws_route_table.public-routes.id
+}
+
+resource "aws_route_table" "private-routes" {
     vpc_id = aws_vpc.main.id
     route {
         cidr_block = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.nat.id
     }
 }
-# resource "aws_subnet" "private-network-1" {
-#     vpc_id = aws_vpc.main.id
-#     cidr_block = "10.0.3.0/24"
-#     map_public_ip_on_launch = "false"
-# }
 
-# resource "aws_subnet" "private-network-2" {
-#     vpc_id = aws_vpc.main.id
-#     cidr_block = "10.0.4.0/24"
-#     map_public_ip_on_launch = "false"
-# }
+resource "aws_subnet" "private-network-1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.4.0/24"
+    map_public_ip_on_launch = "false"
+}
+
+resource "aws_subnet" "private-network-2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.5.0/24"
+    map_public_ip_on_launch = "false"
+}
+
+resource "aws_route_table_association" "private-1" {
+    subnet_id = aws_subnet.private-network-1.id
+    route_table_id = aws_route_table.private-routes.id
+}
+
+resource "aws_route_table_association" "private-2" {
+    subnet_id = aws_subnet.private-network-2.id
+    route_table_id = aws_route_table.private-routes.id
+}
