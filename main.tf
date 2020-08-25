07b5fcc707f85389c05604e5c4f67a25519ad8d3
Pamela Garces <pamelag@codefabric.io>
2020-08-24 21:05:24 -0400

    Added comments to module.

--- main.tf
+++ main.tf
@@ -6,13 +6,16 @@ provider "aws" {
 module "networks" {
     source = "./modules/networks"
     
+    # values for private networks
     nat_cidr_block = var.nat_cidr_block
     private_network_1_cidr_block = var.private_network_1_cidr_block
     private_network_2_cidr_block = var.private_network_2_cidr_block
     
+    # values for public networks
     public_network_1_cidr_block = var.public_network_1_cidr_block
     public_network_2_cidr_block = var.public_network_2_cidr_block
     
+    # values for all networks
     table_routes_cidr_block = var.table_routes_cidr_block
     vpc_cidr_block = var.vpc_cidr_block
 }
